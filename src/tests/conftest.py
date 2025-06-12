d39dd759e55311cc577cc0ef9b10b0ede3dcdce0
Pieter Stokkink <p.stokkink@amsterdam.nl>
2025-06-12 11:19:37 +0200

    add basic test

---
+++ src/tests/conftest.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+from rest_framework.test import APIClient
+
+HERE = Path(__file__).parent
+
+
+@pytest.fixture()
+def api_client() -> APIClient:
+    """Return a client that has unhindered access to the API views"""
+    api_client = APIClient()
+    api_client.default_format = "json"  # instead of multipart
+    return api_client
---
+++ src/tests/test_index.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+def test_index(api_client):
+    response = api_client.get("/")
+    assert response.status_code == 200
+    assert response.data == {"status": "OK"}
