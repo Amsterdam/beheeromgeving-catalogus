83d722c2bb396655a0b2edc168051e13486a0c4d
Pieter Stokkink <p.stokkink@amsterdam.nl>
2026-01-15 15:29:21 +0100

    update initialize_db.sh

--- src/initialize_db.sh
+++ src/initialize_db.sh
@@ -2,5 +2,5 @@
 # Run when INITIALIZE_DB parameter is set and migrations are available.
 if ! ./manage.py migrate --check && "$INITIALIZE_DB" = "true";
 then
-    ./manage.py migrate;
+    uv run ./manage.py migrate;
 fi
