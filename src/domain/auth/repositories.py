72c8164584f251ac25f392898ad4ac7b3d16d1c5
Jasper Jager <j.jager@amsterdam.nl>
2026-02-19 14:13:38 +0100

    Fix auth config for products without name

--- src/domain/auth/repositories.py
+++ src/domain/auth/repositories.py
@@ -15,11 +15,15 @@ class AuthorizationRepository(AbstractAuthRepository):
         self.queryset = orm.Team.objects.all()
 
     def get_config(self) -> AuthorizationConfiguration:
-        team_scopes = {t.id: t.scope for t in self.queryset}
-        product_scopes = {p.id: t.scope for t in self.queryset for p in t.products.all()}
-        product_scopes.update(
-            {p.name.lower(): t.scope for t in self.queryset for p in t.products.all()}
-        )
+        product_scopes = {}
+        team_scopes = {}
+
+        for t in self.queryset:
+            team_scopes[t.id] = t.scope
+            for p in t.products.all():
+                product_scopes[p.id] = t.scope
+                if p.name:
+                    product_scopes[p.name.lower()] = t.scope
         return AuthorizationConfiguration(
             self.admin_role, team_scopes, product_scopes, feature_enabled=self.feature_enabled
         )
